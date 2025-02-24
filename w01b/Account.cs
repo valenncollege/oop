0a88de7e82ecfd6db432b9e901c9e30c209524fb
valenn <s160424066@student.ubaya.ac.id>
2025-02-24 13:30:21 +0700

    Add files via upload

---
+++ w01b/Account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tugas_W1B_Jevon_Valentino_160424066
+{
+    public class Account
+    {
+        private string name;
+        private int phoneNumber;
+        private int ovoID;
+
+        public void TopUp()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Buy()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
